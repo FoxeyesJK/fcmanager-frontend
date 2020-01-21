509a6fddf745226dc9d4228bd44f745e0c37d890
Jake Kwon <jakej.kwon@gmail.com>
2020-01-21 09:20:59 -0500

    Discarded unnecessary pages and component due to a new design

--- src/App.js
+++ src/App.js
@@ -9,12 +9,9 @@ import {
 
 import HomePage from './pages/homepage/homepage.component';
 import Member from './pages/member/member.component';
-import Strategy from './pages/strategy/strategy.component';
 import League from './pages/league/league.component';
 import Fixture from './pages/fixture/fixture.component';
-import Schedule from './pages/schedule/schedule.component';
 import AdminLeague from './pages/admin-league/admin-league.component';
-import AdminMember from './pages/admin-member/admin-member.component';
 import Test from './components/test/test.component';
 import Header from './components/header/header.component';
 import SubHeader from './components/sub-header/sub-header.component';
@@ -36,13 +33,10 @@ class App extends React.Component {
               <Route exact path='/' component={HomePage} />
               <Route exact path='/member' component={Member} />
               <Route exact path='/member-admin' render={(props) => <Member {...props} isAdmin={true} />} />/>
-              <Route exact path='/strategy' component={Strategy} />
               <Route exact path='/league' component={League} />
               <Route exact path='/fixture' component={Fixture} />
               <Route exact path='/fixture-admin' render={(props) => <Fixture {...props} isAdmin={true} />} />
-              <Route exact path='/schedule' component={Schedule} />
               <Route exact path='/admin-league' component={AdminLeague} />
-              <Route exact path='/admin-member' component={AdminMember} />
             </Switch>
           </BodyContainer>
         </Body>
--- src/components/admin-schedule/admin-schedule.component.jsx
+++ src/components/admin-schedule/admin-schedule.component.jsx
@@ -1,108 +1,108 @@
-import React from 'react';
-import axios from 'axios';
-import moment from 'moment';
+// import React from 'react';
+// import axios from 'axios';
+// import moment from 'moment';
 
-import {
-  AdminScheduleContainer,
-  Title,
-  AdminScheduleTable,
-  TableRow,
-  TableHeader,
-  TableBody
-} from './admin-schedule.styles.jsx';
+// import {
+//   AdminScheduleContainer,
+//   Title,
+//   AdminScheduleTable,
+//   TableRow,
+//   TableHeader,
+//   TableBody
+// } from './admin-schedule.styles.jsx';
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
-import CustomIconButton from '../../components/custom-icon-button/custom-icon-button.component';
+// import FormInput from '../form-input/form-input.component';
+// import CustomButton from '../custom-button/custom-button.component';
+// import CustomIconButton from '../../components/custom-icon-button/custom-icon-button.component';
 
 
-import SCHEDULE_DATA from '../../pages/schedule/schedule.data';
+// import SCHEDULE_DATA from '../../pages/schedule/schedule.data';
 
-import ScheduleAddItem from '../admin-schedule-add-item/admin-schedule-add-item.component';
-import ScheduleItem from '../admin-schedule-item/admin-schedule-item.component';
+// import ScheduleAddItem from '../admin-schedule-add-item/admin-schedule-add-item.component';
+// import ScheduleItem from '../admin-schedule-item/admin-schedule-item.component';
 
-export default class AdminSchedule extends React.Component {
-  constructor(props) {
-    super(props);
+// export default class AdminSchedule extends React.Component {
+//   constructor(props) {
+//     super(props);
     
-    this.state = {
-      count: 0,
-      schedules: SCHEDULE_DATA
-    }
-  }
-  // state = {
-  //   persons: [],
-  // };
-  // componentDidMount() {
-  //   axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
-  //     console.log(res);
-  //     this.setState({ persons: res.data });
-  //   })
-  // }
+//     this.state = {
+//       count: 0,
+//       schedules: SCHEDULE_DATA
+//     }
+//   }
+//   // state = {
+//   //   persons: [],
+//   // };
+//   // componentDidMount() {
+//   //   axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+//   //     console.log(res);
+//   //     this.setState({ persons: res.data });
+//   //   })
+//   // }
 
-  handleChange = event => {
-    const { value, name } = event.target;
+//   handleChange = event => {
+//     const { value, name } = event.target;
 
-    this.setState({ 
-      [name]: value //dynamically setting value
-    });
-  }
+//     this.setState({ 
+//       [name]: value //dynamically setting value
+//     });
+//   }
 
-  handleSubmit = event => {
-    event.preventDefault();
+//   handleSubmit = event => {
+//     event.preventDefault();
 
-    const schedule = {
-      date: this.state.date,
-      title: this.state.title,
-      location: this.state.location
-    }
+//     const schedule = {
+//       date: this.state.date,
+//       title: this.state.title,
+//       location: this.state.location
+//     }
 
-    axios.post(`https://jsonplaceholder.typicode.com/schedules`, { schedule })
-      .then(res => {
-      });
-  }
+//     axios.post(`https://jsonplaceholder.typicode.com/schedules`, { schedule })
+//       .then(res => {
+//       });
+//   }
 
-  handleClick(id) {
-    this.setState({})
-  }
+//   handleClick(id) {
+//     this.setState({})
+//   }
 
-  addScheduleRow = () => {
-    this.setState(prevState => {
-      return {count: prevState.count + 1}
-    });
-  }
+//   addScheduleRow = () => {
+//     this.setState(prevState => {
+//       return {count: prevState.count + 1}
+//     });
+//   }
 
 
-  render() {
-    const { schedules, count } = this.state;
-    return (
-        <AdminScheduleContainer>
-            <Title>SCHEDULES</Title>
-              <AdminScheduleTable>
-                <TableRow className='table-row'>
-                  <TableHeader>Date</TableHeader>
-                  <TableHeader>Time</TableHeader>
-                  <TableHeader>Title</TableHeader>
-                  <TableHeader>Location</TableHeader>
-                  <TableHeader><CustomIconButton type='' handleClick={this.addScheduleRow} /></TableHeader>
-                  <TableHeader><CustomIconButton type='add' handleClick={this.addScheduleRow} /></TableHeader>
-                </TableRow>
-                <TableBody onSubmit={this.handleSubmit}>
-                {
-                  this.state.count > 0 ? 
-                  <ScheduleAddItem /> : null
-                }
-                {
-                  schedules.map(({ id, ...otherScheduleProps }) => (
-                    <ScheduleItem key={id} id={id} {...otherScheduleProps} handleClick={this.handleClick} />
-                  ))
-                }
-                </TableBody>
-            </AdminScheduleTable>
+//   render() {
+//     const { schedules, count } = this.state;
+//     return (
+//         <AdminScheduleContainer>
+//             <Title>SCHEDULES</Title>
+//               <AdminScheduleTable>
+//                 <TableRow className='table-row'>
+//                   <TableHeader>Date</TableHeader>
+//                   <TableHeader>Time</TableHeader>
+//                   <TableHeader>Title</TableHeader>
+//                   <TableHeader>Location</TableHeader>
+//                   <TableHeader><CustomIconButton type='' handleClick={this.addScheduleRow} /></TableHeader>
+//                   <TableHeader><CustomIconButton type='add' handleClick={this.addScheduleRow} /></TableHeader>
+//                 </TableRow>
+//                 <TableBody onSubmit={this.handleSubmit}>
+//                 {
+//                   this.state.count > 0 ? 
+//                   <ScheduleAddItem /> : null
+//                 }
+//                 {
+//                   schedules.map(({ id, ...otherScheduleProps }) => (
+//                     <ScheduleItem key={id} id={id} {...otherScheduleProps} handleClick={this.handleClick} />
+//                   ))
+//                 }
+//                 </TableBody>
+//             </AdminScheduleTable>
 
-        </AdminScheduleContainer>
-    )
-  }
-}
+//         </AdminScheduleContainer>
+//     )
+//   }
+// }
 
-//https://www.youtube.com/watch?v=oQnojIyTXb8
+// //https://www.youtube.com/watch?v=oQnojIyTXb8
