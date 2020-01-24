1a3f769059190c93db640910f47f5eb0d32aa778
Jake Kwon <jake.kw@iottie.com>
2020-01-24 17:22:42 -0500

    Removed Homepage test data

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -8,24 +8,8 @@ import {
   StyledSelect
 } from './homepage.styles.jsx';
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
-
 const HomePage = () => (
   <HomePageContainer>
-      {/* <Dropdown
-    placeholder='Select Country'
-    search
-    selection
-    options={countryOptions}
-  /> */}
-  <StyledSelect options={options} />
     <Test />
   </HomePageContainer>
 );
